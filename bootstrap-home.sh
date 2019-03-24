86283d2ad7b46ae0becabbaeb644f2fd6df8a781
Kyle Rooker <rookerka@gmail.com>
2019-03-24 15:54:28 -0400

    Minor update to bootstrap script.

--- bootstrap-home.sh
+++ bootstrap-home.sh
@@ -16,8 +16,8 @@ scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 dotHomeDir="$scriptDir/home/user"
 
 # Locations where files will be symlinked
-configDir="$HOME/test/.config"
-localBinDir="$HOME/test/bin"
+configDir="$HOME/.config"
+localBinDir="$HOME/bin"
 
 # Create the directories if they don't exist
 if mkdir -p $configDir $localBinDir; then
@@ -29,18 +29,16 @@ fi
 
 # link config
 info "Linking files and directories to $HOME/.config..."
-for file in $dotHomeDir/.config/*
-    do
-        source=$(basename $file)
-        symlink $file $configDir/$source
-    done
+for file in $dotHomeDir/.config/*; do
+    source=$(basename $file)
+    symlink $file $configDir/$source
+done
 unset file
 
 # link bin scripts
 info "Linking files and directories to $HOME/bin..."
-for file in $dotHomeDir/bin/*
-    do
-        source=$(basename $file)
-        symlink $file $localBinDir/$source
-    done
+for file in $dotHomeDir/bin/*; do
+    source=$(basename $file)
+    symlink $file $localBinDir/$source
+done
 unset file
